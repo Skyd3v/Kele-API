3a303aecd6a8b4eba165613c47a62cb80b2f107c
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-12-08 21:15:06 +0000

    add mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,6 +11,11 @@ class Kele
 
     def get_me
         response = self.class.get('/users/me', headers: { 'authorization' => @auth_token })
-        JSON.parse(response.body)
+        JSON.parse(response.body).to_a
     end
+
+    def get_mentor_availability(mentor_id)
+        response = self.class.get('/mentors/' + mentor_id.to_s + '/student_availability', headers: { 'authorization' => @auth_token })
+        JSON.parse(response.body).to_a
+  end
   end
