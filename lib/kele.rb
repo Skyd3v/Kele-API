fc1a9c2a8c60f829557d41f27dd6c59b9909c2dc
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-12-09 23:08:02 +0000

    fixes due to token errors

--- lib/kele.rb
+++ lib/kele.rb
@@ -46,12 +46,13 @@ class Kele
    response = self.class.post('/checkpoint_submissions',
                              headers: { "authorization" => @auth_token },
                              body: {
-                               "assignment_branch": assignment_branch,
-                               "assignment_commit_link": assignment_commit_link,
-                               "checkpoint_id": checkpoint_id,
-                               "comment": comment,
-                               "enrollment_id": enrollment_id
+                               assignment_branch: assignment_branch,
+                               assignment_commit_link: assignment_commit_link,
+                               checkpoint_id: checkpoint_id,
+                               comment: comment,
+                               enrollment_id: enrollment_id
                              })
    JSON.parse(response.body)
 
+end
 end
